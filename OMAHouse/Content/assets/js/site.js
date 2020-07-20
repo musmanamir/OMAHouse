d6b3adb9a6a150f5037387e0484802cbe2d86002
Ammad Amir <muhammadammad2222@gmail.com>
2020-07-21 00:17:04 +0500

    added layout

---
+++ OMAHouse/Content/assets/js/site.js
@@ -0,0 +1,19 @@
+﻿
+function DisplayTextbox(type)
+{
+    console.log(type);
+    if (type == 1) {
+        $("#CreditTextBox").show();
+        $("#DebitTextBox").hide();
+    }
+    else {
+        $("#DebitTextBox").show();
+        $("#CreditTextBox").hide();
+    }
+}
+
+$('.radioAmountType').on('change', function () {
+
+    alert($('radioAmountType:checked').closest('label').text());
+
+});
